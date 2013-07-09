014494122be21fb15a094266b48f782aa27e5d38
huolg <1244002@qq.com>
2013-07-09 15:06:48 +0800

    guo

--- app/controllers/weixin/main_controller.rb
+++ app/controllers/weixin/main_controller.rb
@@ -1,15 +1,27 @@
 #encoding:utf-8
-class Weixin::MainController < Weixin::ApplicationController
+class Weixin::MainController < Weixin::BaseController
+  layout proc{ |controller| get_layout }
   #平台首页
   def index
     if @subdomain == "" || @subdomain == "www"
     else
       if kind = Kindergarten.find_by_number(@subdomain)
-        redirect_to :controller=>"/my_school/main",:action=>"index"
-        return
+#        redirect_to :controller=>"/weixin/main",:action=>"index"
+#        return
       else
         @no_kind = true
       end
     end
+  end
+
+  #绑定用户
+  def bind_user
+    
+  end
+  
+  private
+  #设置模板
+  def get_layout
+    session[:weixin_layout] ||= "#{load_layout}_weixin"
   end
 end
