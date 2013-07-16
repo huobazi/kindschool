9c72d9629cb15170329e72864a68373f361de008
huolg <1244002@qq.com>
2013-07-16 10:40:39 +0800

    guo

--- app/models/message_entry.rb
+++ app/models/message_entry.rb
@@ -1,6 +1,10 @@
 class MessageEntry < ActiveRecord::Base
-  attr_accessible :content, :message_id, :phone, :receiver_id, :receiver_name, :sms, :status
+  attr_accessible :content, :message_id, :phone, :receiver_id, :receiver_name, :sms, :status,:read_status
 
   belongs_to :message
   belongs_to :receiver, :class_name => "User",:foreign_key=>:receiver_id
+
+  def read_status_label
+    self.read_status ? "已读" : "未读"
+  end
 end
