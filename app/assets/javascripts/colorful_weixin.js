fa4072d1c76dea5fe9611c7f0ec932ffe6e8499f
huolg <1244002@qq.com>
2013-07-23 18:08:40 +0800

    guo

--- app/assets/javascripts/colorful_weixin.js
+++ app/assets/javascripts/colorful_weixin.js
@@ -14,3 +14,19 @@
 //= require twitter/bootstrap
 //= require jquery_ujs
 //= require jquery.ui.all
+$(function(){
+    function tabs(tabTit,on,tabCon){
+        $(tabCon).each(function(){
+            $(this).children().eq(0).show();
+        });
+        $(tabTit).each(function(){
+            $(this).children().eq(0).addClass(on);
+        });
+        $(tabTit).children().hover(function(){
+            $(this).addClass(on).siblings().removeClass(on);
+            var index = $(tabTit).children().index(this);
+            $(tabCon).children().eq(index).show().siblings().hide();
+        });
+    }
+    tabs(".tab-hd","active",".tab-bd");
+});
