a1992868e1d2c73ccd888e83fea832cbeedf96ff
yinsigan <ganweiliang886@gmail.com>
2013-07-23 16:06:44 +0800

    fix topic model

--- app/controllers/weixin/messages_controller.rb
+++ app/controllers/weixin/messages_controller.rb
@@ -1,16 +1,7 @@
 #encoding:utf-8
 class Weixin::MessagesController < Weixin::ManageController
   def index
-    @send_date = [["全部时间",""],["最近3天","1"],["最近7天","2"]]
-    if send_date = params[:send_date]
-      if send_date == "1"
-        start_time = Date.today - 3.day
-        params[:messages][:send_date_greater_than] = start_time
-      elsif send_date == "2"
-        start_time = Date.today - 7.day
-        params[:messages][:send_date_greater_than] = start_time
-      end
-    end
+    
     params[:messages] = {} if params[:messages].blank?
     params[:messages][:message_entries_receiver_id_equals] = current_user.id
     @messages = Message.search(params[:messages] || {}).where("messages.kindergarten_id=:kind_id AND messages.status = :status",
--- app/controllers/weixin/topics_controller.rb
+++ app/controllers/weixin/topics_controller.rb
@@ -1,7 +1,24 @@
 #encoding:utf-8
 class Weixin::TopicsController < Weixin::ManageController
   def index
-    @topics = @kind.topics.page(params[:page] || 1).per(10)
+    @send_date = [["全部时间",""],["最近3天","1"],["最近7天","2"]]
+    if send_date = params[:send_date]
+      if send_date == "1"
+        start_time = Date.today - 3.day
+        params[:messages][:send_date_greater_than] = start_time
+      elsif send_date == "2"
+        start_time = Date.today - 7.day
+        params[:messages][:send_date_greater_than] = start_time
+      end
+    end
+    params[:messages] = {} if params[:messages].blank?
+    params[:messages][:message_entries_receiver_id_equals] = current_user.id
+    @messages = Message.search(params[:messages] || {}).where("messages.kindergarten_id=:kind_id AND messages.status = :status", {:kind_id=>@kind.id,:status=>1}).page(params[:page] || 1).per(10).order("messages.send_date DESC")
+    unless params[:category_id].blank?
+      @topics = @kind.topics.where(:topic_category_id => params[:category_id].to_i).page(params[:page] || 1).per(10)
+    else
+      @topics = @kind.topics.page(params[:page] || 1).per(10)
+    end
   end
 
   def show
