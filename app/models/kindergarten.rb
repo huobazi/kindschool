8a44165a666df8e23cf9d60159edf4c460a282ab
huolg <1244002@qq.com>
2013-07-26 11:56:55 +0800

    guo

--- app/models/kindergarten.rb
+++ app/models/kindergarten.rb
@@ -5,6 +5,7 @@ class Kindergarten < ActiveRecord::Base
 
   validates :name,:presence => true, :uniqueness => true, :length => { :maximum => 100}
   validates :number,:presence => true, :uniqueness => true, :length => { :maximum => 100}
+  validates :note, :length => { :maximum => 800}
   STATUS_DATA = {"0"=>"正常","1"=>"锁定"}
   WEIXIN_STATUS_DATA = {"0"=>"未授权绑定","1"=>"已授权绑定"}
 
