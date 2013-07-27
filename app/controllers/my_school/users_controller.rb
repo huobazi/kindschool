fa4922c2c77b1e22fa7784e3c853aa7609a2f871
huolg <1244002@qq.com>
2013-07-27 16:42:35 +0800

    guo

--- app/controllers/my_school/users_controller.rb
+++ app/controllers/my_school/users_controller.rb
@@ -124,7 +124,7 @@ class MySchool::UsersController < MySchool::ManageController
   #设置能发送短信
   def set_send_sms
     if user = User.find_by_id_and_kindergarten_id(params[:id],@kind.id)
-      user.update_attribute(:is_send, !user.user.is_send)
+      user.update_attribute(:is_send, !user.is_send)
       flash[:success] = "设置成功。"
     else
       flash[:error] = "需要设置的用户不存在"
@@ -138,7 +138,7 @@ class MySchool::UsersController < MySchool::ManageController
   #设置能收到短信
   def set_gather_sms
     if user = User.find_by_id_and_kindergarten_id(params[:id],@kind.id)
-      user.update_attribute(:is_receive, !user.user.is_receive)
+      user.update_attribute(:is_receive, !user.is_receive)
       flash[:success] = "设置成功。"
     else
       flash[:error] = "需要设置的用户不存在"
