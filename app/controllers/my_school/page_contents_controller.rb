f80cb56fc56850cd7462bd7bfed0892aa7637422
yinsigan <ganweiliang886@gmail.com>
2013-07-29 17:51:47 +0800

    zhangfang

--- app/controllers/my_school/page_contents_controller.rb
+++ app/controllers/my_school/page_contents_controller.rb
@@ -181,6 +181,17 @@ class  MySchool::PageContentsController < MySchool::ManageController
         elsif params[:tp] == "contact_us"
           @entry = ContentEntry.find(params[:entry_id])
           @entry.update_attributes(:content=>(params[:content]||""))
+        elsif params[:tp] == "official_website_admissions_information"
+        @entry = ContentEntry.find(params[:entry_id])
+        if @entry.number == "official_website_admissions_title"
+          @entry.content =params[:content]
+          @entry.title=params[:title]
+          @entry.save
+        elsif @entry.number == "official_website_admissions_mid_title"
+          @entry.content =params[:mid_content]
+          @entry.title=params[:mid_title]
+          @entry.save
+        end
         elsif params[:tp] == "official_website_home"
           @entry = ContentEntry.find(params[:entry_id])
             if @entry.number == "official_home_pub_img"
@@ -238,42 +249,32 @@ class  MySchool::PageContentsController < MySchool::ManageController
           end
           unless params[:img].blank?
             if @entry = content_entries.find_by_number('official_website_about_us_img')
-             # @entry.page_img.destroy
              unless @entry.page_img.blank?
                @entry.page_img.update_attributes(:uploaded_data=> params[:img])
              else
                img = PageImg.new(:uploaded_data=> params[:img])
                @entry.page_img = img
              end
-              # @entry.save!
             end
           end
           unless params[:img_top].blank?
             if @entry = content_entries.find_by_number('official_website_about_us_img_top')
-             # @entry.page_img.destroy
-             # img = PageImg.new(:uploaded_data=> params[:img_top])
-             # @entry.page_img = img
              unless @entry.page_img.blank?
                @entry.page_img.update_attributes(:uploaded_data=> params[:img_top])
              else
                img = PageImg.new(:uploaded_data=> params[:img_top])
                @entry.page_img = img
              end
-              # @entry.save
             end
           end
           unless params[:img_bottom].blank?
             if @entry = content_entries.find_by_number('official_website_about_us_img_bottom')
-             # @entry.page_img.destroy             
-             # img = PageImg.new(:uploaded_data=> params[:img_bottom])
-             # @entry.page_img = img
              unless @entry.page_img.blank?
                @entry.page_img.update_attributes(:uploaded_data=> params[:img_bottom])
              else
                img = PageImg.new(:uploaded_data=> params[:img_bottom])
                @entry.page_img = img
              end
-              # @entry.save
             end
           end
         end
