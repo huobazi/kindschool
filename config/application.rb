464487dc8e0e70e769cc28edc6efbef3ce224520
yinsigan <ganweiliang886@gmail.com>
2013-08-06 14:31:37 +0800

    zhangfang

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 #encoding:utf-8
 require File.expand_path('../boot', __FILE__)
 
+require 'csv'
+
 require 'rails/all'
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
