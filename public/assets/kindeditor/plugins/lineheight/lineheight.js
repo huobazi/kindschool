a96bf582d747f5bc2bad1232f6c3116b431d3a2c
huolg <1244002@qq.com>
2013-08-12 17:31:37 +0800

    guo

--- public/assets/kindeditor/plugins/lineheight/lineheight.js
+++ public/assets/kindeditor/plugins/lineheight/lineheight.js
@@ -6,33 +6,4 @@
 * @site http://www.kindsoft.net/
 * @licence http://www.kindsoft.net/license.php
 *******************************************************************************/
-
-KindEditor.plugin('lineheight', function(K) {
-	var self = this, name = 'lineheight', lang = self.lang(name + '.');
-	self.clickToolbar(name, function() {
-		var curVal = '', commonNode = self.cmd.commonNode({'*' : '.line-height'});
-		if (commonNode) {
-			curVal = commonNode.css('line-height');
-		}
-		var menu = self.createMenu({
-			name : name,
-			width : 150
-		});
-		K.each(lang.lineHeight, function(i, row) {
-			K.each(row, function(key, val) {
-				menu.addItem({
-					title : val,
-					checked : curVal === key,
-					click : function() {
-						self.cmd.toggle('<span style="line-height:' + key + ';"></span>', {
-							span : '.line-height=' + key
-						});
-						self.updateState();
-						self.addBookmark();
-						self.hideMenu();
-					}
-				});
-			});
-		});
-	});
-});
+KindEditor.plugin("lineheight",function(e){var t=this,a="lineheight",i=t.lang(a+".");t.clickToolbar(a,function(){var n="",l=t.cmd.commonNode({"*":".line-height"});l&&(n=l.css("line-height"));var o=t.createMenu({name:a,width:150});e.each(i.lineHeight,function(a,i){e.each(i,function(e,a){o.addItem({title:a,checked:n===e,click:function(){t.cmd.toggle('<span style="line-height:'+e+';"></span>',{span:".line-height="+e}),t.updateState(),t.addBookmark(),t.hideMenu()}})})})})});
--- public/assets/kindeditor/plugins/preview/preview.js
+++ public/assets/kindeditor/plugins/preview/preview.js
@@ -6,26 +6,4 @@
 * @site http://www.kindsoft.net/
 * @licence http://www.kindsoft.net/license.php
 *******************************************************************************/
-
-KindEditor.plugin('preview', function(K) {
-	var self = this, name = 'preview', undefined;
-	self.clickToolbar(name, function() {
-		var lang = self.lang(name + '.'),
-			html = '<div style="padding:10px 20px;">' +
-				'<iframe class="ke-textarea" frameborder="0" style="width:708px;height:400px;"></iframe>' +
-				'</div>',
-			dialog = self.createDialog({
-				name : name,
-				width : 750,
-				title : self.lang(name),
-				body : html
-			}),
-			iframe = K('iframe', dialog.div),
-			doc = K.iframeDoc(iframe);
-		doc.open();
-		doc.write(self.fullHtml());
-		doc.close();
-		K(doc.body).css('background-color', '#FFF');
-		iframe[0].contentWindow.focus();
-	});
-});
+KindEditor.plugin("preview",function(e){var t=this,i="preview";t.clickToolbar(i,function(){var n=(t.lang(i+"."),'<div style="padding:10px 20px;"><iframe class="ke-textarea" frameborder="0" style="width:708px;height:400px;"></iframe></div>'),a=t.createDialog({name:i,width:750,title:t.lang(i),body:n}),l=e("iframe",a.div),o=e.iframeDoc(l);o.open(),o.write(t.fullHtml()),o.close(),e(o.body).css("background-color","#FFF"),l[0].contentWindow.focus()})});
