511737bc3db6c85f23086b7cc9abd3cc73b81d40
yinsigan <ganweiliang886@gmail.com>
2013-08-12 18:15:28 +0800

    zhangfang

--- app/controllers/my_school/personal_sets_controller.rb
+++ app/controllers/my_school/personal_sets_controller.rb
@@ -6,10 +6,10 @@ class MySchool::PersonalSetsController < MySchool::ManageController
     userrole = current_user.get_users_ranges[:tp]
     if userrole == :student
       @flag="student"
-    elsif userrole = :teacher
+    elsif userrole == :teacher
       @flag= "teacher"
     end
-  	@sets = current_user.personal_sets
+  	@sets = current_user.personal_sets.page(params[:page] || 1).per(10)
   end
   
   def new
