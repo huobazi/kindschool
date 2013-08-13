fd95db88c5f0e797bb1dd6348662fdfe00c829cc
yinsigan <ganweiliang886@gmail.com>
2013-08-13 17:10:28 +0800

    growth_record stat

--- app/controllers/my_school/statistics_controller.rb
+++ app/controllers/my_school/statistics_controller.rb
@@ -3,7 +3,7 @@ class  MySchool::StatisticsController < MySchool::ManageController
 
   def growth_record
     if params[:start_at].present? and params[:end_at].present?
-      @growth_records = @kind.growth_records.where("start_at > ? and end_at < ?", params[:start_at], params[:end_at])
+      @growth_records = @kind.growth_records.where("start_at >= ? and end_at <= ?", params[:start_at], params[:end_at])
       @home_growth_record_count = @growth_records.where(tp: 1).count
       @garden_growth_record_count = @growth_records.where(tp: 0).count
     else
