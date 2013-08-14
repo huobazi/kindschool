83f4009509e7dea514e6a2e4c49d281bfc5ea52f
yinsigan <ganweiliang886@gmail.com>
2013-08-14 14:56:55 +0800

    growth_record stat

--- app/controllers/my_school/statistics_controller.rb
+++ app/controllers/my_school/statistics_controller.rb
@@ -7,11 +7,24 @@ class  MySchool::StatisticsController < MySchool::ManageController
       @home_growth_record_count = @growth_records.where(tp: 1).count
       @garden_growth_record_count = @growth_records.where(tp: 0).count
     else
-      @growth_record_count        = @kind.growth_records.count
+      @growth_records        = @kind.growth_records.where("start_at >= ? and end_at <= ?", Time.now.beginning_of_week, Time.now.end_of_week)
       @home_growth_record_count   = @kind.growth_records.where(tp: 1).count
       @garden_growth_record_count = @kind.growth_records.where(tp: 0).count
     end
 
+   @growth_records_count = @kind.growth_records.count
+   @home_growth_records_count = @kind.growth_records.where(tp: 1).count
+   @garden_growth_records_count = @kind.growth_records.where(tp: 0).count
+
+   @cur_begginning_of_week = Time.now.beginning_of_week.year.to_s + "-" + Time.now.beginning_of_week.month.to_s + "-" + Time.now.beginning_of_week.day.to_s
+   @cur_end_of_week = Time.now.end_of_week.year.to_s + "-" + Time.now.end_of_week.month.to_s + "-" + Time.now.end_of_week.day.to_s
+
+  end
+
+  def kind_stat
+    @grades_count = @kind.grades.count
+    @squads_count = @kind.squads.count
+    @student_infos_count = @kind.student_infos.count
   end
 
 end
