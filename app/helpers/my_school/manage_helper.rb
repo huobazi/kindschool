837c59f77a6b38f051d13c7624fc4c461434f485
huolg <1244002@qq.com>
2013-08-16 11:26:53 +0800

    guo

--- app/helpers/my_school/manage_helper.rb
+++ app/helpers/my_school/manage_helper.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 module MySchool::ManageHelper
 	def choose_operate_show(controller_view)
-		session[:operates].include?(controller_view)
+		(session[:operates]||[]).include?(controller_view)
 	end
 	def calculated_figures
 		str = current_user.name + current_user.role.try(:name).to_s
