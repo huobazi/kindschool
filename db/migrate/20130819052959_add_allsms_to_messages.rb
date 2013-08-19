947f03b112360cc492aca7a072c8d9c382da75fc
huolg <1244002@qq.com>
2013-08-19 11:05:21 +0800

    guo

--- db/migrate/20130819052959_add_allsms_to_messages.rb
+++ db/migrate/20130819052959_add_allsms_to_messages.rb
@@ -1,4 +1,4 @@
-class AddAllSmsTomessages < ActiveRecord::Migration
+class AddAllsmsToMessages < ActiveRecord::Migration
   def change
     add_column :messages, :allsms, :boolean, :default => false  #是否所有都发短信
     add_column :sms_records, :sms_count, :integer, :default => 1  #短信数量，70个字为一条短信
