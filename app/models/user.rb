dbe780faf13932b0b90e3538f10987af37e8d9bf
yinsigan <ganweiliang886@gmail.com>
2013-08-19 11:50:40 +0800

    zhangfang

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
 
 
   validates :password, :confirmation=> { :allow_blank=> true }, :length=>{:maximum=>20,:minimum=>6} ,:if => :password_required?
-  validates :phone,:presence => true,:uniqueness => true, :format=> {:with=> /^(\+\d+-)?[1-9]{1}[0-9]{10}$/, :message=> "手机格式不正确"}#{ :scope => :kindergarten_id}
+  validates :phone,:presence => true, :format=> {:with=> /^(\+\d+-)?[1-9]{1}[0-9]{10}$/, :message=> "手机格式不正确"}#{ :scope => :kindergarten_id}
   validates :name, :kindergarten_id,:presence => true
   # validates :login,:presence => true ,format: {with: /^[w][y][s]/, message: "注册名不能以#{PRE_STUDENT}"} , :if => :role_student?
   validates :login, :length=>{:maximum=>20,:minimum=>5}, :format=> {:with => /^[a-z0-9_\-@\.]*$/i,:message=>"帐号只允许使用5到20位字符的英文、数字和下划线组合"}, :if => :role_student?
@@ -57,7 +57,7 @@ class User < ActiveRecord::Base
   def automatically_generate_account
    #kind = self.kindergarten
    #user = kind.users.order("id desc").where(:tp=>0).first
-   user = User.where(:tp=>0).order("id desc").first
+   user = User.with_deleted.where(:tp=>0).order("id desc").first
    if !user.blank?
     self.login = user.login.next_number
    else
