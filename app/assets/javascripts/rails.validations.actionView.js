358772a34f85baea768ce7be1d13526fc03032da
yinsigan <ganweiliang886@gmail.com>
2013-08-27 10:01:19 +0800

    fix bug

---
+++ app/assets/javascripts/rails.validations.actionView.js
@@ -0,0 +1,11 @@
+window.ClientSideValidations.formBuilders['ActionView::Helpers::FormBuilder'] = {
+  add: function(element, settings, message) {
+    // custom add code here
+    console.log("addd");
+  },
+
+  remove: function(element, settings) {
+    // custom remove code here
+    console.log("remove");
+  }
+}
