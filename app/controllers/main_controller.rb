a68fdc1b205894bd6a180af8540c29769e66eb15
huolg <1244002@qq.com>
2013-08-27 09:15:20 +0800

    guo

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,12 +3,14 @@ class MainController < ApplicationController
   #平台首页
   def index
     if @subdomain.blank? || @subdomain == "www"
+      render :layout=>"weiyi"
     else
       if kind = Kindergarten.find_by_number(@subdomain)
         redirect_to :controller=>"/my_school/main",:action=>"index"
         return
       else
         @no_kind = true
+        render :layout=>"weiyi"
       end
     end
   end
