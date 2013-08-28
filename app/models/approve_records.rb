5949a351d67ca73336e48918a34211f327d3e81b
yinsigan <ganweiliang886@gmail.com>
2013-08-28 12:08:50 +0800

    zf

--- app/models/approve_records.rb
+++
@@ -1,7 +0,0 @@
-#encoding:utf-8
-#审核记录
-class ApproveRecords < ActiveRecord::Base
-  attr_accessible :resource_id, :resource_type, :status
-  belongs_to :resource, :polymorphic => true #指定审核的类型/对象
-  STATUS = { 0=>"不通过",1=> "通过"}
-end
