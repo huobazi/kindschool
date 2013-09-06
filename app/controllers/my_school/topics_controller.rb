9bc0e5cc02db8b4071d6a417e8ffb521a66aa88d
yinsigan <ganweiliang886@gmail.com>
2013-09-06 14:22:21 +0800

    topic squad

--- app/controllers/my_school/topics_controller.rb
+++ app/controllers/my_school/topics_controller.rb
@@ -160,9 +160,11 @@ class  MySchool::TopicsController < MySchool::ManageController
     if current_user.get_users_ranges[:tp] == :student
       @topic = @kind.topics.where(:creater_id => current_user.id).find_by_id(params[:id])
     elsif current_user.get_users_ranges[:tp] == :teachers
-      @topic = @kind.topics.where("squad_id in (select squad_id from teachers where staff_id = ?) or creater_id = ? or squad_id is NULL", current_user.staff.id, current_user.id).find_by_id(params[:id])
+      @topic = @kind.topics.where("squad_id in (select squad_id from teachers where staff_id = ?) or creater_id = ?", current_user.staff.id, current_user.id).find_by_id(params[:id])
+      @squads = current_user.get_users_ranges[:squads]
     else
       @topic = @kind.topics.find_by_id(params[:id])
+      @grades = @kind.grades
     end
 
     if @topic.nil?
@@ -178,10 +180,14 @@ class  MySchool::TopicsController < MySchool::ManageController
         params[:topic][:squad_id] = current_user.student_info.squad_id
       end
     end
+
+    if params[:visible].presence == "all"
+      params[:topic][:squad_id] = "NULL"
+    end
     if current_user.get_users_ranges[:tp] == :student
       @topic = @kind.topics.find_by_id_and_creater_id(params[:id], current_user.id)
     elsif current_user.get_users_ranges[:tp] == :teachers
-      @topic = @kind.topics.where("squad_id in (select squad_id from teachers where staff_id = ?) or creater_id = ? or squad_id is NULL", current_user.staff.id, current_user.id).find_by_id(params[:id])
+      @topic = @kind.topics.where("squad_id in (select squad_id from teachers where staff_id = ?) or creater_id = ?", current_user.staff.id, current_user.id).find_by_id(params[:id])
     else
       @topic = @kind.topics.find_by_id(params[:id])
     end
