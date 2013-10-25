19f9ba65b50dc6582d3bcb5396a70c3c7181fec9
yinsigan <ganweiliang886@gmail.com>
2013-10-25 17:41:16 +0800

    finish growth record stat

--- app/controllers/my_school/statistics_controller.rb
+++ app/controllers/my_school/statistics_controller.rb
@@ -99,11 +99,14 @@ class  MySchool::StatisticsController < MySchool::ManageController
   # 老师日常管理统计
   def teacher_stat
     @staffs = @kind.staffs
+    unless params[:start_at].blank? and params[:end_at].blank?
+      @syslogs = SysLog.select("count(1) as syslogcount, sys_logs.url_options, sys_logs.user_id, sys_logs.method").joins("INNER JOIN staffs as st on(sys_logs.user_id = st.user_id)").joins("INNER JOIN users as u on u.id = st.user_id").where("u.kindergarten_id=? and sys_logs.created_at >= ? and sys_logs.created_at <= ?", @kind.id, params[:start_at], params[:end_at] ).group("sys_logs.url_options, sys_logs.user_id").group_by {|syslog| syslog.user_id}
+      @start_at = params[:start_at]
+      @end_at = params[:end_at]
+    else
+      @syslogs = SysLog.select("count(1) as syslogcount, sys_logs.url_options, sys_logs.user_id, sys_logs.method").joins("INNER JOIN staffs as st on(sys_logs.user_id = st.user_id)").joins("INNER JOIN users as u on u.id = st.user_id").where("u.kindergarten_id=?", @kind.id).group("sys_logs.url_options, sys_logs.user_id").group_by {|syslog| syslog.user_id}
+    end
     if request.xhr?
-      unless params[:start_at].blank? and params[:end_at].blank?
-        @start_at = params[:start_at]
-        @end_at = params[:end_at]
-      end
       render "my_school/statistics/teacher_stat.js.erb"
     else
       render "my_school/statistics/teacher_stat"
--- app/models/sys_log.rb
+++ app/models/sys_log.rb
@@ -5,102 +5,12 @@ class SysLog < ActiveRecord::Base
   belongs_to :kindergarten
   belongs_to :user #, :class_name => "User", :foreign_key => "user_id"
 
-  class << self
-    attr_accessor :syslog_list
-    attr_accessor :syslogkeys
-  end
-
-  def self.init_syslog_list
-    self.syslog_list = {
-      "topics" => {
-        "name" => "贴子",
-        "count" => 0,
-        "text" => ""
-      },
-      "messages" => {
-        "name" => "消息",
-        "count" => 0,
-        "text" => ""
-      },
-      "garden_growth_records" => {
-        "name" => "宝宝在园成长记录",
-        "count" => 0,
-        "text" => ""
-      },
-      "growth_records" => {
-        "name" => "宝宝在家成长记录",
-        "count" => 0,
-        "text" => ""
-      },
-      "comment" => {
-        "name" => "评论",
-        "count" => 0,
-        "text" => ""
-      },
-      "cook_books" => {
-        "name" => "菜谱",
-        "count" => 0,
-        "text" => ""
-      },
-      "physical_records" => {
-        "name" => "体检记录",
-        "count" => 0,
-        "text" => ""
-      },
-      "seedlings" => {
-        "name" => "疫苗记录",
-        "count" => 0,
-        "text" => ""
-      },
-      "teaching_plans" => {
-        "name" => "教学计划",
-        "count" => 0,
-        "text" => ""
-      },
-      "albums" => {
-        "name" => "相册集锦",
-        "count" => 0,
-        "text" => ""
-      },
-      "activities" => {
-        "name" => "活动",
-        "count" => 0,
-        "text" => ""
-      }
-    }
-    self.syslogkeys = SYSLOGS.keys.map {|sk| sk.split("/").last }
-  end
-
   # 计算并输出各种操作对象的数量
   def self.output_syslog(teacher, start_at=nil, end_at=nil)
-    self.init_syslog_list
     if start_at.present? && end_at.present?
-      syslogs = SysLog.where("user_id = ? and created_at >= ? and created_at <= ?", teacher.try(:user_id), start_at, end_at)
+      syslogs = SysLog.where("user_id = ?", teacher.try(:user_id), start_at, end_at).group(:url_options)
     else
       syslogs = SysLog.where(:user_id => teacher.try(:user_id))
-    end
-    unless syslogs.blank?
-      syslogs.each do |syslog|
-        record_controller = syslog.url_options.split("controller: ").last.chomp
-        record_name = record_controller.split("/").last.chomp
-        record_actions = /action:\s(\w+)\b/.match(syslog.url_options)[1]
-        record_method = syslog.method
-        if self.syslogkeys.include?(record_name) && SYSLOGS.keys.include?(record_controller)
-          self.syslog_list[record_name]["count"] += 1
-          if self.syslog_list[record_name]["text"].blank?
-            self.syslog_list[record_name]["text"] = SYSLOGS[record_controller][record_actions][record_method]
-          end
-        end
-      end
-      text = ""
-      (self.syslog_list.values || []).each do |sl|
-        if sl["count"] > 0
-          text += "<p>#{sl["text"]}数量:<span class='text-info'>#{sl["count"]}<span><p>"
-        end
-      end
-      return text
-    else
-      return "<p class='muted'>没有任何操作记录</p>"
     end
   end
 
@@ -113,7 +23,7 @@ class SysLog < ActiveRecord::Base
         logtrail.url_chinese=syslog
         logtrail.kindergarten_id=kind_id
         logtrail.user_id=current_user_id
-        logtrail.url_options=url
+        logtrail.url_options="#{controller}/#{action}"
         logtrail.method=method
         logtrail.original_url=original_url
         logtrail.remote_ip=remote_ip
