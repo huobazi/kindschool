fea69ae2ae5014fc1d7bfa8177b8f459363e6be1
huolg <1244002@qq.com>
2013-10-30 13:38:58 +0800

    guo

--- app/admin/weiyi_config.rb
+++ app/admin/weiyi_config.rb
@@ -2,6 +2,20 @@
 ActiveAdmin.register WeiyiConfig do
   menu :parent => "微壹平台管理", :priority => 1
 
+  member_action :delete_img, :method => :get do
+    if(@weiyi_config = WeiyiConfig.find_by_id(params[:id])) && (page_img = @weiyi_config.page_imgs.find(params[:img_id]))
+      if page_img.destroy
+        flash[:notice] ="操作成功."
+      else
+        flash[:error] = "操作失败."
+      end
+      redirect_to(:controller=>"/admin/weiyi_configs", :action=>:show,:id=>params[:id])
+    else
+      flash[:error] = "记录不存在."
+      redirect_to(:controller=>"/admin/weiyi_configs", :action=>:index)
+    end
+  end
+
   index do
     div do
       raw '编号：<br/>"web_weiyi_about"为微一官网的关于我们,"web_weiyi_interact"为微一官网的家园互动,"web_weiyi_contact"为微一官网的联系我们,<br/>
@@ -21,10 +35,20 @@ ActiveAdmin.register WeiyiConfig do
       f.input :number, :required => true
     end
     f.inputs "配置内容" do
-      if ["web_weiyi_about","web_weiyi_interact","web_weiyi_contact"].include?(f.object.number)
+      if ["web_weiyi_about","web_weiyi_interact","web_weiyi_contact","web_garden_about","web_garden_kindergarten"].include?(f.object.number)
         f.kindeditor :content,:allowFileManager => false
       else
         f.input :content
+      end
+    end
+    if f.object.number == "web_garden_kindergarten"
+      f.inputs "上传滚动照片"  do
+        f.has_many :page_imgs do |page_img|
+          if !page_img.object.new_record?
+            page_img.input :created_at, :as => :string, :input_html => {:disabled => true }
+          end
+          page_img.input :uploaded_data,:as=>:file
+        end
       end
     end
     f.actions
@@ -35,6 +59,17 @@ ActiveAdmin.register WeiyiConfig do
       row :number
       row :content do
         raw(record.content)
+      end
+      if record.number == "web_garden_kindergarten"
+        div do
+          br
+          panel "家园互动滑动图片" do
+            table_for(record.page_imgs) do |t|
+              t.column("图片") {|item| raw "<img src='#{item.public_filename(:tiny)}' />"}
+              t.column("操作") {|item| link_to "删除" ,:action=>:delete_img,:id=>record.id,:img_id=>item.id}
+            end
+          end
+        end
       end
     end
   end
--- app/models/weiyi_config.rb
+++ app/models/weiyi_config.rb
@@ -4,4 +4,9 @@ class WeiyiConfig < ActiveRecord::Base
   attr_accessible :number, :content
 
   validates :number, :presence => true,:uniqueness=>true    #必须输入/不为空
+
+  has_many :page_imgs, :class_name => "PageImg", :as => :resource, :dependent => :destroy
+
+  attr_accessible :page_imgs_attributes
+  accepts_nested_attributes_for :page_imgs
 end
