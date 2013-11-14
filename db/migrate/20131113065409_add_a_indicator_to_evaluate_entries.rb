f3676c1d65ff1ab693cf042b9ad982d21c589235
yinsigan <ganweiliang886@gmail.com>
2013-11-14 11:18:08 +0800

    zhangfang

--- db/migrate/20131113065409_add_a_indicator_to_evaluate_entries.rb
+++
@@ -1,6 +0,0 @@
-class AddAIndicatorToEvaluateEntries < ActiveRecord::Migration
-  def change
-    add_column :evaluate_entries, :a_indicator, :string
-    add_column :evaluate_entries, :b_indicator, :string
-  end
-end
