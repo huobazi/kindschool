09bfdb061e1328f7ddc0df1dea0d822492d651d1
yinsigan <ganweiliang886@gmail.com>
2013-11-14 17:24:34 +0800

    zhangfang

--- app/models/evaluate_entry.rb
+++ app/models/evaluate_entry.rb
@@ -5,7 +5,7 @@ class EvaluateEntry < ActiveRecord::Base
   #:article_case #案条
   attr_accessible :b_indicator,:a_indicator,:kindergarten_id, :note,:evaluate_id,:name,:article_case,:sequence,:note,:self_note
   belongs_to :kindergarten
-  belongs_to :evaluate
+  belongs_to :evaluate#, :dependent => :destroy
   has_many :evaluate_vtocs, :dependent => :destroy
   validates :name, :presence => true
 end
