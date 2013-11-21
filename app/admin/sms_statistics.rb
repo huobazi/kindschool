126338d7b8a0b9c6c882f50ad8ec748f94c00c55
yinsigan <ganweiliang886@gmail.com>
2013-11-21 17:21:32 +0800

    zhangfang

--- app/admin/sms_statistics.rb
+++ app/admin/sms_statistics.rb
@@ -3,20 +3,16 @@
 ActiveAdmin.register_page "SmsStatistics" do
 menu :parent => "短信计费", :priority => 14
  content :title => proc{ I18n.t("active_admin.dashboard") } do
-  controller do
-  	def index
-      @sms_records = SmsRecord.select('sum(sms_records.sms_count) as sum_count').joins("LEFT JOIN  message_entries ON(message_entries.id = sms_records.message_entry_id)").group('message_entries.message_id,kindergarten_id').where(:status=>1).order("created_at DESC")
-      # render :partial => "/admin/sms_statistics/index" 
-  	  index!
+  # collection_action :kind_sms_records, :method => :get, :title => "幼儿园" do
+  # end
+  columns do
+  	column do
+  	  div do
+            br
+            @records = SmsRecord.search(params[:sms_record]).select('sms_records.*, messages.tp ,sum(sms_records.sms_count) as sum_count').joins("LEFT JOIN  message_entries ON(message_entries.id = sms_records.message_entry_id) LEFT JOIN messages ON(message_entries.message_id = messages.id)").group('sms_records.kindergarten_id').group('messages.tp').page(params[:page] || 1).per(10).order("sms_records.created_at DESC") 
+            render :partial => "/admin/sms_statistics/index" ,:locals=>{:sms_records=>@records}
+          end
+     end
     end
-  end
-  # columns do
-  # 	column do
-  # 	  div do
-  #           br
-  #           render :partial => "/admin/sms_statistics/index"
-  #         end
-  #    end
-  #   end
 end
 end
