ca5092d82f08e811b270f14f0e24d4b4a0677acf
yinsigan <ganweiliang886@gmail.com>
2013-12-02 11:36:47 +0800

    zhangfang

--- app/admin/sms_statistics.rb
+++ app/admin/sms_statistics.rb
@@ -9,7 +9,7 @@ menu :parent => "短信计费", :priority => 14
   	column do
   	  div do
             br
-            @records = SmsRecord.search(params[:sms_record]).select('sms_records.*, messages.tp ,sum(sms_records.sms_count) as sum_count').joins("LEFT JOIN  message_entries ON(message_entries.id = sms_records.message_entry_id) LEFT JOIN messages ON(message_entries.message_id = messages.id)").group('sms_records.kindergarten_id').group('messages.tp').page(params[:page] || 1).per(10).order("sms_records.created_at DESC") 
+            @records = SmsRecord.search(params[:sms_record]).select('sms_records.*, messages.tp ,sum(sms_records.sms_count) as sum_count').joins("LEFT JOIN  message_entries ON(message_entries.id = sms_records.message_entry_id) LEFT JOIN messages ON(message_entries.message_id = messages.id)").group('sms_records.kindergarten_id').group('messages.tp').where(:status=>1).page(params[:page] || 1).per(10).order("sms_records.created_at DESC")
             render :partial => "/admin/sms_statistics/index" ,:locals=>{:sms_records=>@records}
           end
      end
