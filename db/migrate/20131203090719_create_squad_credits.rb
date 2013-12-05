90381d02ec4617cc88b9ed9ca67fcb60af05cc57
yinsigan <ganweiliang886@gmail.com>
2013-12-05 10:52:58 +0800

    zhangfang

--- db/migrate/20131203090719_create_squad_credits.rb
+++ db/migrate/20131203090719_create_squad_credits.rb
@@ -2,7 +2,7 @@ class CreateSquadCredits < ActiveRecord::Migration
   def change
     create_table :squad_credits do |t|
       t.integer :squad_id
-      t.float :credit
+      t.float :credit,:default=>0
       t.integer :kindergarten_id
 
       t.timestamps
