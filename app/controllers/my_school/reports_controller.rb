dced301a14f0979027525cfd332245a22a029c21
yinsigan <ganweiliang886@gmail.com>
2013-12-06 17:06:53 +0800

    add report link to news and topic view page

--- app/controllers/my_school/reports_controller.rb
+++ app/controllers/my_school/reports_controller.rb
@@ -2,7 +2,7 @@
 class  MySchool::ReportsController < MySchool::ManageController
   def create
     @report = current_user.reports.new
-    if @report.create_report(params[:resource_type], params[:resource_id])
+    if @report.create_report(params[:resource_type], params[:resource_id], @kind)
       flash[:success] = "举报成功"
       redirect_to params[:url]
     end
