b532f274303c4ff1b3c329b15e0cdd048c742b60
yinsigan <ganweiliang886@gmail.com>
2013-12-06 10:34:47 +0800

    add report at admin

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,15 @@
+#encoding:utf-8
 class Report < ActiveRecord::Base
-  attr_accessible :content, :informants, :process, :resource_id, :resource_type
+  attr_accessible :content, :informants_id, :process, :resource_id, :resource_type
+
+  belongs_to :informants, :class_name => 'User', :foreign_key => "informants_id"
+
+  validate :content, :resource_type, :resource_id, :informants_id, :presence => true
+
+  PROCESS = {'0' => '未处理', '1' => '已解决', '2' => '正在处理'}
+
+  def process_label
+    Report::PROCESS[self.process.to_s]
+  end
+
 end
--- db/migrate/20131206013055_create_reports.rb
+++ db/migrate/20131206013055_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
       t.text :content
-      t.integer :informants
+      t.integer :informants_id
       t.integer :process, :default => 0
       t.integer :resource_id
       t.string :resource_type
@@ -10,7 +10,7 @@ class CreateReports < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :reports, :informants
+    add_index :reports, :informants_id
 
     add_index :reports, ["resource_type", "resource_id"], :name => "report_resources"
   end
