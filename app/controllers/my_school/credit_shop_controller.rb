4717bdba60491632c469589ed88549f96eebd1df
huolg <1244002@qq.com>
2013-12-12 17:33:39 +0800

    guo

--- app/controllers/my_school/credit_shop_controller.rb
+++ app/controllers/my_school/credit_shop_controller.rb
@@ -5,11 +5,24 @@ class MySchool::CreditShopController < MySchool::ManageController
   before_filter :get_shop_config
 
   def index
-   @tags = Product.tag_counts
+    @tags = Product.tag_counts
   end
 
   def products
-   @products = Product.search(params[:product] || {}).page(params[:page] || 1).per(25)
+    @serarch_type = params[:serarch_type]
+    if @serarch =  params[:serarch]
+      case @serarch
+      when "credit"
+        then
+        puts "=========0"
+        products = @serarch_type == "descend" ? Product.descend_by_credit : Product.ascend_by_credit
+      when "price"
+        then
+        puts "=========1"
+        products = @serarch_type == "descend" ? Product.descend_by_price : Product.ascend_by_price
+      end
+    end
+    @products = (products || Product).search(params[:product] || {}).page(params[:page] || 1).per(25)
   end
   
   private
