5af759ce1d0b9e5f3e8916ab805abfd5f8140b88
yinsigan <ganweiliang886@gmail.com>
2013-12-13 17:33:53 +0800

    zhangfang

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,14 @@
 #encoding:utf-8
 #订单
 class Order < ActiveRecord::Base
-  attr_accessible :amount, :credit, :express_code, :kindergarten_id, :note, :number, :postage, :shipment_at, :status, :user_id
+  attr_accessible  :delivery_address_id,:phone,:address, :address_info,:amount, :credit, :express_code, :kindergarten_id, :note, :number, :postage, :shipment_at, :status, :user_id
   belongs_to :kindergarten
   belongs_to :user
   has_many :order_infos#, :class_name=>"OrderInfo"
   validates :status,:presence => true
   validates :number,:presence => true
   validates :number, :uniqueness => true
-
+  validates :phone,:presence => true
+  validates :address,:presence => true
 
 end
