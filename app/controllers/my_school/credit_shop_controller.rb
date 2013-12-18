c633d8bc1409586d48657862246cd9eb5d799ac3
huolg <1244002@qq.com>
2013-12-18 16:37:25 +0800

    guo

--- app/controllers/my_school/credit_shop_controller.rb
+++ app/controllers/my_school/credit_shop_controller.rb
@@ -32,6 +32,10 @@ class MySchool::CreditShopController < MySchool::ManageController
     end
   end
   
+  def show_merchant
+    @merchant = Merchant.find_by_id(params[:id])
+  end
+  
   #添加购物车
   def add_to_cart
     product = Product.find(params[:id])
