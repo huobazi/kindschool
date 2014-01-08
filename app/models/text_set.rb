d5093599a65ba9be475cef91e50668d4fa9d6805
huolg <1244002@qq.com>
2014-01-08 16:11:27 +0800

    guo

--- app/models/text_set.rb
+++ app/models/text_set.rb
@@ -1,5 +1,5 @@
 class TextSet < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :content
+  attr_accessible :content,:tp,:audio
   has_one :personal_set, :class_name => "PersonalSet", :as => :resource, :dependent => :destroy
 end
