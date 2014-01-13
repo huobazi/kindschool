a6e15589e5c5814903f6450ed96cb3bc3d76f176
yinsigan <ganweiliang886@gmail.com>
2014-01-13 17:13:20 +0800

    fix

--- app/admin/help_movies.rb
+++ app/admin/help_movies.rb
@@ -4,21 +4,21 @@ ActiveAdmin.register HelpMovie do
   menu :parent => "微壹平台", :priority => 1
   index do
     column :content do |obj|
-        raw(obj.content)
-      end
+      raw(obj.content)
+    end
     column :name
-    
+
     default_actions
   end
-  
+
   filter :name
 
   form do |f|
-  	f.inputs "视频配置" do
-  	 f.input :name
+    f.inputs "视频配置" do
+      f.input :name
     end
-  	f.inputs "视频内容" do
-  	 f.kindeditor :content,:allowFileManager => false
+    f.inputs "视频内容" do
+      f.kindeditor :content,:allowFileManager => false
     end
     f.actions
   end
