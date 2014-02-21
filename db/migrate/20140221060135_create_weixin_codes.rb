bf534f8b9244a022568141b780615c09328ed6bd
huolg <1244002@qq.com>
2014-02-21 14:43:58 +0800

    guo

--- db/migrate/20140221060135_create_weixin_codes.rb
+++ db/migrate/20140221060135_create_weixin_codes.rb
@@ -5,28 +5,25 @@ class CreateWeixinCodes < ActiveRecord::Migration
       t.integer :weixin_tp
       t.integer :user_id
       t.integer :kindergarten_id
-
       t.timestamps
     end
     WeixinCode.reset_column_information
-    User.all.each do |u|
-      if u.weiyi_code
-        w1 = WeixinCode.new
-        w1.user_id = u.id
-        w1.weixin_code = u.weiyi_code
-        w1.kindergarten_id = u.kindergarten_id
-        w1.weixin_tp = 1
-        w1.save
-
+    User.all.each do |record|
+      unless record.weiyi_code.blank?
+        weixin_code = WeixinCode.new
+        weixin_code.user_id = record.id
+        weixin_code.weixin_code = record.weiyi_code
+        weixin_code.kindergarten_id = record.kindergarten_id
+        weixin_code.weixin_tp = 1
+        weixin_code.save
       end
-
-      if u.weixin_code
-        w2 = WeixinCode.new
-        w2.user_id = u.id
-        w2.weixin_code = u.weixin_code
-        w2.kindergarten_id = u.kindergarten_id
-        w2.weixin_tp = 0
-        w2.save
+      unless record.weixin_code.blank?
+        weiyi_code = WeixinCode.new
+        weiyi_code.user_id = record.id
+        weiyi_code.weixin_code = record.weixin_code
+        weiyi_code.kindergarten_id = record.kindergarten_id
+        weiyi_code.weixin_tp = 0
+        weiyi_code.save
       end
     end
   end
